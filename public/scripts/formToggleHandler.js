99550c1a5c049eab4d71485e76882bffaf61362e
So Hyun (Esther) Choi <estherchoi.sh@gmail.com>
2023-02-01 20:33:10 +0000

    Add sass

--- public/scripts/formToggleHandler.js
+++ public/scripts/formToggleHandler.js
@@ -1,9 +1,22 @@
 const toggleForm = () => {
   const $form = $("main").children(".new-tweet").children("form");
   if ($form.is(":hidden")) {
+    console.log($(window).scrollTop());
+    console.log($("header").height());
+    if ($(window).scrollTop() > $("header").height()) {
+      $("html, body").animate(
+        {
+          scrollTop: $form.offset().top,
+        },
+        1000
+      );
+    }
+
     $form.slideDown("slow");
+    $("#toggle-label").html("Close Form");
   } else {
     $form.slideUp("slow");
+    $("#toggle-label").html("Show Form");
   }
 };
 
